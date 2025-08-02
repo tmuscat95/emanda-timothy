615a46cd1abeab7fd000fce18ff142466ec11cf5
Timothy Muscat <timothy.muscat@pateplay.com>
2025-08-02 16:59:58 +0300

    Removed cors.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  // Enable CORS for the frontend development server; remove
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
   await app.listen(3000);
   
 }
